003f3d5f2164209a711ad337f49f6d443e746a15
maenard <maenardaboabo065@gmail.com>
2024-02-18 04:19:23 +0800

    changed colorscheme to gruvbox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,9 @@
 return {
-  { "Mofiqul/dracula.nvim" },
-
+  { "ellisonleao/gruvbox.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "dracula",
+      colorscheme = "gruvbox",
     },
   },
 }
